2f272359a7969796c2582f189e78a2f4bc0a6a28
stephanygrace <stephanytayong73@gmail.com>
2023-03-27 21:10:10 +0800

    Initial Commit

---
+++ src/Contact.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Contact = () => {
+    return (
+        <div className="container-center-horizontal">
+            <div className="frame-1 screen">
+                <div className="overlap-group">
+                    <h1 className="newsletter">
+                        Subscribe
+                        <br />
+                        to our Newsletter
+                    </h1>
+                <div className="overlap-group1">
+                    <p className="updates">
+                        Get the latest update about AWU Token.
+                    </p>
+                <div className="rectangle-25">
+                    <p className="email-address">
+                        Enter your email address here
+                    </p>
+                </div>
+                <div className="rectangle-26">
+                    <img className="send-1" src="send-1.png" alt="send 1" />
+
+                </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Contact
